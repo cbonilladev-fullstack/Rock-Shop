d864be31802bfc341e60091d49a87566b3cbe3b2
cabonilla <christian.bonilla01@gmail.com>
2021-07-06 18:53:52 -0700

    Added live shop button and button linking.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,17 @@
 import { Button, Container, Grid, Typography } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CartItem from './CartItem/CartItem';
 import useStyles from './styles';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
 	const classes = useStyles();
 
 	const EmptyCart = () => {
 		return (
-		<Typography variant="subtitle1">You have no rocks in your shopping cart. Get started!</Typography>
+		<Typography variant="subtitle1">You have no rocks in your shopping cart,
+			<Link to="/" className={classes.link}>go add some!</Link>
+		</Typography>
 		)
 	}
 
@@ -18,7 +21,7 @@ const Cart = ({ cart }) => {
 			<Grid container spacing={3}>
 				{cart.line_items.map((item) => (
 					<Grid item xs={12} sm={4} key={item.id}>
-						<CartItem item={item}/>
+						<CartItem item={item} onUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} />
 					</Grid>
 				))}
 			</Grid>
@@ -32,7 +35,9 @@ const Cart = ({ cart }) => {
 						size="large"
 						type="button"
 						variant="contained"
-						color="secondary">Empty Cart</Button>
+						color="secondary"
+						onClick={handleEmptyCart}
+						>Empty Cart</Button>
 					<Button
 						className={classes.checkoutButton}
 						size="large"
