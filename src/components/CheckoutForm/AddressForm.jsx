c73a26b810c0be34cd2994c7b9572d59e591cded
cabonilla <christian.bonilla01@gmail.com>
2021-07-07 13:10:00 -0700

    Completed first form using React Hook Forms. Not bad.

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import FormField from './CustomTextField';
 
 const AddressForm = () => {
+	const methods = useForm();
+
 	return (
 		<div>
-			AddressForm
+			<Typography variant="h6" gutterBottom>Shipping Address</Typography>
+			<FormProvider {...methods}>
+				<form onSubmit=''>
+					<Grid container spacing={3}>
+						<FormField required name='firstName' label='First Name'/>
+						<FormField required name='lastName' label='Last Name'/>
+						<FormField required name='address1' label='Address'/>
+						<FormField required name='email' label='Email'/>
+						<FormField required name='city' label='City'/>
+						<FormField required name='zip' label='ZIP | Postal Code'/>
+					</Grid>
+				</form>
+			</FormProvider>
 		</div>
 	)
 }
---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,25 @@
+import { Grid, TextField } from '@material-ui/core';
+import React from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+
+const FormInput = ({ name, label, required }) => {
+	const { control } = useFormContext();
+
+	return (
+		<Grid item xs={12} sm={6}>
+         <Controller
+            control={control}
+            name={name}
+            render = {({ field })=> (
+                <TextField
+                    fullWidth
+                    label={label}
+                    required
+                />
+            )}
+         />
+		</Grid>
+	)
+}
+
+export default FormInput
